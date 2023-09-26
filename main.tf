f0763a6d333242284b04eeac0ab45541a0355b52
CFelt22 <charles.felteau@gmail.com>
2023-09-26 18:28:59 +0000

    #25 restructure root module part 1

--- main.tf
+++ main.tf
@@ -1,46 +1,18 @@
-terraform {
-  cloud {
-    organization = "CFelteau"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  region     = ""
-  access_key = ""
-  secret_key = ""
-}
-
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   length           = 32
   special          = false
   lower            = true
   upper            = false
 }
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
-}
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+  bucket = random_string.bucket_name.result
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = "var.user_uuid"
+  }
 }
