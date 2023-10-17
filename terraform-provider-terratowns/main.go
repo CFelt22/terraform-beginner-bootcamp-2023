2d43d7f7bd69bf87cc85f6d78c8b655eba4b77c1
CFelt22 <charles.felteau@gmail.com>
2023-10-17 18:18:55 +0000

    #43 wip setting up the skeleton for creating a custom terraform provider

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -2,12 +2,12 @@
 package main
 
 // fmt is short for format. Imports the fmt package which contains functions for formatted I/O.
-import {
+import (
+	//"log"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
-}
+)
 
 // Defines the `main` function, the entry point of the application. When you run the program, it starts executing from this function.
 func main() {
@@ -25,30 +25,39 @@ func Provider() *schema.Provider {
 	p = &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
 
-		}
+		},
 		DataSourcesMap: map[string]*schema.Resource{
 
-		}
-		Schema: map[string]*schema.Resource{
+		},
+		Schema: map[string]*schema.Schema{
 			"endpoint": {
 				Type: schema.TypeString,
 				Required: true,
-				Description: "The endpoint for the external service"
+				Description: "The endpoint for the external service",
 			},
 			"token": {
 				Type: schema.TypeString,
 				Sensitive: true, // mark the token as sensitive to hide it in the logs
 				Required: true,
-				Description: "Bearer token for authorization"
+				Description: "Bearer token for authorization",
 			},
 			"user_uuid": {
 				Type: schema.TypeString,
 				Required: true,
 				Description: "UUID for configuration",
-				ValidateFunc: validateUUID
-			}
-		}
+				//ValidateFunc: validateUUID,
+			},
+		},
 	}
-	p.ConfigureContextFunc = providerConfigure
+	//p.ConfigureContextFunc = providerConfigure
 	return p
 }
+
+//func validateUUID(v interface{}, k string) (ws []string, errors []errors) {
+//	log.Print('validateUUID:start')
+//	value := v.(string)
+//	if _,err = uuid.Parse(value); err != nil {
+//		errors = append(error, fmt.Errorf("Invalid UUID format"))
+//	}
+//	log.Print('validateUUID:end')
+//}
